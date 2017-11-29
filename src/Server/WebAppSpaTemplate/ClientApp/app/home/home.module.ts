bb3d9d47629da05709e6f91db75fea38204578be
John Pankowicz <johnpankowicz@hotmail.com>
2017-11-28 19:14:26 -0500

    Got the 'Register, Login, etc' menu to display properly and not interfere with Angular's navbar

--- src/Server/WebAppSpaTemplate/ClientApp/app/home/home.module.ts
+++ src/Server/WebAppSpaTemplate/ClientApp/app/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
   providers: []
