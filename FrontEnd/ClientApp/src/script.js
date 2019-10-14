7a77f681d4e489b8b1d441d5abb669c3de00506a
John Pankowicz <johnpankowicz@hotmail.com>
2019-10-14 17:01:06 -0400

    fix responsiveness of dash-header

--- FrontEnd/ClientApp/src/script.js
+++ FrontEnd/ClientApp/src/script.js
@@ -3,8 +3,6 @@
 $(document).ready(() => {
   addResizeListeners();
   setSidenavListeners();
-  // setUserDropdownListener();
-  renderChart();
   setMenuClickListener();
   setSidenavCloseListener();
 });
@@ -14,117 +12,6 @@ const sidenavEl = $('.sidenav');
 const gridEl = $('.grid');
 const SIDENAV_ACTIVE_CLASS = 'sidenav--active';
 const GRID_NO_SCROLL_CLASS = 'grid--noscroll';
-
-function toggleClass(el, className) {
-  if (el.hasClass(className)) {
-    el.removeClass(className);
-  } else {
-    el.addClass(className);
-  }
-}
-
-// User avatar dropdown functionality
-/* function setUserDropdownListener() {
-  const userAvatar = $('.header__avatar');
-
-  userAvatar.on('click', function(e) {
-    const dropdown = $(this).children('.dropdown');
-    toggleClass(dropdown, 'dropdown--active');
-  });
-}
- */
-
- // Sidenav list sliding functionality
-function setSidenavListeners() {
-  const subHeadings = $('.navList__subheading');
-   console.log('subHeadings: ', subHeadings);
-  const SUBHEADING_OPEN_CLASS = 'navList__subheading--open';
-  const SUBLIST_HIDDEN_CLASS = 'subList--hidden';
-
-  subHeadings.each((i, subHeadingEl) => {
-    $(subHeadingEl).on('click', (e) => {
-      const subListEl = $(subHeadingEl).siblings();
-
-      // Add/remove selected styles to list category heading
-      if (subHeadingEl) {
-        toggleClass($(subHeadingEl), SUBHEADING_OPEN_CLASS);
-      }
-
-      // Reveal/hide the sublist
-      if (subListEl && subListEl.length === 1) {
-        toggleClass($(subListEl), SUBLIST_HIDDEN_CLASS);
-      }
-    });
-  });
-}
-
-// Draw the chart
-function renderChart() {
-  const chart = AmCharts.makeChart( "chartdiv", {
-    "type": "serial",
-    "theme": "light",
-    "dataProvider": [ {
-      "month": "Jan",
-      "visits": 2025
-    }, {
-      "month": "Feb",
-      "visits": 1882
-    }, {
-      "month": "Mar",
-      "visits": 1809
-    }, {
-      "month": "Apr",
-      "visits": 1322
-    }, {
-      "month": "May",
-      "visits": 1122
-    }, {
-      "month": "Jun",
-      "visits": 1114
-    }, {
-      "month": "Jul",
-      "visits": 984
-    }, {
-      "month": "Aug",
-      "visits": 711
-    }, {
-      "month": "Sept",
-      "visits": 665
-    }, {
-      "month": "Oct",
-      "visits": 580
-    } ],
-    "valueAxes": [ {
-      "gridColor": "#FFFFFF",
-      "gridAlpha": 0.2,
-      "dashLength": 0
-    } ],
-    "gridAboveGraphs": true,
-    "startDuration": 1,
-    "graphs": [ {
-      "balloonText": "[[category]]: <b>[[value]]</b>",
-      "fillAlphas": 0.8,
-      "lineAlpha": 0.2,
-      "type": "column",
-      "valueField": "visits"
-    } ],
-    "chartCursor": {
-      "categoryBalloonEnabled": false,
-      "cursorAlpha": 0,
-      "zoomable": false
-    },
-    "categoryField": "month",
-    "categoryAxis": {
-      "gridPosition": "start",
-      "gridAlpha": 0,
-      "tickPosition": "start",
-      "tickLength": 20
-    },
-    "export": {
-      "enabled": false
-    }
-  });
-}
 
 function toggleClass(el, className) {
   if (el.hasClass(className)) {
@@ -160,5 +47,30 @@ function setSidenavCloseListener() {
   $('.sidenav__brand-close').on('click', function(e) {
     toggleClass(sidenavEl, SIDENAV_ACTIVE_CLASS);
     toggleClass(gridEl, GRID_NO_SCROLL_CLASS);
+  });
+}
+
+// I will most likely replace his navigation menu with one base on Angular Material.
+ // Sidenav list sliding functionality
+ function setSidenavListeners() {
+  const subHeadings = $('.navList__subheading');
+   console.log('subHeadings: ', subHeadings);
+  const SUBHEADING_OPEN_CLASS = 'navList__subheading--open';
+  const SUBLIST_HIDDEN_CLASS = 'subList--hidden';
+
+  subHeadings.each((i, subHeadingEl) => {
+    $(subHeadingEl).on('click', (e) => {
+      const subListEl = $(subHeadingEl).siblings();
+
+      // Add/remove selected styles to list category heading
+      if (subHeadingEl) {
+        toggleClass($(subHeadingEl), SUBHEADING_OPEN_CLASS);
+      }
+
+      // Reveal/hide the sublist
+      if (subListEl && subListEl.length === 1) {
+        toggleClass($(subListEl), SUBLIST_HIDDEN_CLASS);
+      }
+    });
   });
 }
