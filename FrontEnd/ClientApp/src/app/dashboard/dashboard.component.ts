cae8931f71277b8a680ec263a352cccd1dfa16bc
John Pankowicz <johnpankowicz@hotmail.com>
2019-12-08 15:20:14 -0500

    sidenav closes when mobile. Add close icon to sidenav

--- FrontEnd/ClientApp/src/app/dashboard/dashboard.component.ts
+++ FrontEnd/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -5,6 +5,11 @@ import { Component, HostListener } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver, BreakpointState  } from '@angular/cdk/layout';
 import {FormBuilder, FormGroup} from '@angular/forms';
+
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, OnDestroy} from '@angular/core';
+
+
 import { SidenavComponent } from './dash-sidenav/sidenav-container/sidenav.component';
 import {NavService} from './dash-sidenav/sidenav-menu4/nav.service';
 
@@ -14,36 +19,55 @@ import {NavService} from './dash-sidenav/sidenav-menu4/nav.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  isHandset = false;
-  isLarge = false;
-  viewsize: number = 1;
-  public results1$;
-  public results2$;
-  public innerWidth;
-  public innerHeight;
-  orientation:string;
+  // isHandset = false;
+  // isLarge = false;
+  // viewsize: number = 1;
+  // public results1$;
+  // public results2$;
+  // public innerWidth;
+  // public innerHeight;
+  // orientation:string;
+
+
+  // myob = {'(max-width: 1000px)': true, '(max-width: 1500px)': true, '(max-width: 2000px)': true}
+
+  // setMode(value) {
+  //   this.options.value.mode = value;
+  // }
 
   options: FormGroup;
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
 
-  myob = {'(max-width: 1000px)': true, '(max-width: 1500px)': true, '(max-width: 2000px)': true}
+  constructor(
+    public navService: NavService,
+    private breakpointObserver: BreakpointObserver,
+    fb: FormBuilder,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher
+    ) {
 
-  setMode(value) {
-    this.options.value.mode = value;
-  }
-
-  constructor(public navService: NavService, private breakpointObserver: BreakpointObserver, fb: FormBuilder) {
     this.options = fb.group({
       bottom: 0,
-      fixed: false,
+      fixed: true,
       top: 0,
       mode: 'side',
       hasBackdrop: false
     });
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+
   }
 
-  ngOnInit() {
-    this.results1$ = this.breakpointObserver.observe('(max-width: 350px)')
-    this.results2$ = this.breakpointObserver.observe(['(max-width: 350px)', '(max-width: 450px)'])
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  // ngOnInit() {
+    // this.results1$ = this.breakpointObserver.observe('(max-width: 350px)')
+    // this.results2$ = this.breakpointObserver.observe(['(max-width: 350px)', '(max-width: 450px)'])
 
 /*
     Using Breakpoints.Handset, I get the following results:
@@ -56,53 +80,58 @@ export class DashboardComponent {
       From 751 to 960, it's in landscape mode and thinks it is a handset again.
       From 961 - max, it doesn't
 */
-  this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .subscribe((state: BreakpointState) => {
-      if (state.matches) {
-        this.isHandset = true;
-      } else {
-        this.isHandset = false;
-      }
-    });
+  // this.breakpointObserver
+  //   .observe(Breakpoints.Handset)
+  //   .subscribe((state: BreakpointState) => {
+  //     if (state.matches) {
+  //       this.isHandset = true;
+  //     } else {
+  //       this.isHandset = false;
+  //     }
+  //   });
 
-  this.breakpointObserver
-    .observe(['(max-width: 1000px)', '(max-width: 1500px)',
-     '(max-width: 2000px)'])
-    .subscribe((state: BreakpointState) => {
-      console.log(state);
-      // Set viewsize from 1 (smallest) to 4 (largest)
-      if (!state.breakpoints['(max-width: 2000px)']) {
-        this.viewsize = 4;
-      } else {
-        if (!state.breakpoints['(max-width: 1500px)']){
-          this.viewsize = 3;
-        } else {
-            if (!state.breakpoints['(max-width: 1000px)']){
-              this.viewsize = 2;
-          } else {
-              this.viewsize = 1;
-            }
-        }
-      }
-    });
-  }
+  // this.breakpointObserver
+  //   .observe(['(max-width: 1000px)', '(max-width: 1500px)',
+  //    '(max-width: 2000px)'])
+  //   .subscribe((state: BreakpointState) => {
+  //     console.log(state);
+  //     // Set viewsize from 1 (smallest) to 4 (largest)
+  //     if (!state.breakpoints['(max-width: 2000px)']) {
+  //       this.viewsize = 4;
+  //     } else {
+  //       if (!state.breakpoints['(max-width: 1500px)']){
+  //         this.viewsize = 3;
+  //       } else {
+  //           if (!state.breakpoints['(max-width: 1000px)']){
+  //             this.viewsize = 2;
+  //         } else {
+  //             this.viewsize = 1;
+  //           }
+  //       }
+  //     }
+  //   });
+  // }
 
-  @HostListener('window:resize', ['$event'])
-onResize(event) {
-  this.innerWidth = window.innerWidth;
-  this.innerHeight = window.innerHeight;
-  this.orientation = (this.innerWidth > this.innerHeight) ? "landscape" : "portrait";
+//   @HostListener('window:resize', ['$event'])
+// onResize(event) {
+//   this.innerWidth = window.innerWidth;
+//   this.innerHeight = window.innerHeight;
+//   this.orientation = (this.innerWidth > this.innerHeight) ? "landscape" : "portrait";
+// }
+
+// GetCols() : number {
+//   return this.viewsize * 3;
+// }
+// GetColspan(s, l) : number {
+//     //return this.isLarge ? 3 * l : 3 * s;
+//     return 3;
+//   }
+
+
 }
 
-GetCols() : number {
-  return this.viewsize * 3;
-}
-GetColspan(s, l) : number {
-    //return this.isLarge ? 3 * l : 3 * s;
-    return 3;
-  }
-}
+
+
   // LgMdSm(large: number, middle: number, small: number) {
   // }
 
