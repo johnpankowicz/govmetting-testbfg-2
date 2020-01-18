ea732f93e7d7d52bfaa2a009c300e728cd0c867a
John Pankowicz <johnpankowicz@hotmail.com>
2020-01-18 09:59:36 -0500

    Add AspAuthController for direct access to account functions

---
+++ BackEnd/Web_App/Features/AspAuth/AspAuthController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GM.WebApp.Features.AspAuth
+{
+    public class AspAuthController : Controller
+    {
+        private readonly ILogger<AspAuthController> _logger;
+
+        public AspAuthController(ILogger<AspAuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+}
