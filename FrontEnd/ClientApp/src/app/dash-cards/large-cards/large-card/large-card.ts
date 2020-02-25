d20962558f41f2a80d388b8cf205f018014cfa3e
John Pankowicz <johnpankowicz@hotmail.com>
2020-02-25 12:07:48 -0500

    Allow click on titles to open/close small & large cards

--- FrontEnd/ClientApp/src/app/dash-cards/large-cards/large-card/large-card.ts
+++ FrontEnd/ClientApp/src/app/dash-cards/large-cards/large-card/large-card.ts
@@ -1,6 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { UserSettingsService, UserSettings, LocationType } from '../../../user-settings.service';
 
 const NoLog = true;  // set to false for console logging
 
@@ -9,28 +7,16 @@ const NoLog = true;  // set to false for console logging
   templateUrl: './large-card.html',
   styleUrls: ['./large-card.scss']
 })
-export class LargeCardComponent implements OnInit {
+export class LargeCardComponent {
   private ClassName: string = this.constructor.name + ": ";
 
   @Input() title: string;
   @Input() icon: string;
   @Input() iconcolor: string;
   @Input() tooltip: string;
-  userSettingsService: UserSettingsService;
-  subscription: Subscription;
-  // enabled: boolean = true;
+  panelOpenState: boolean = false;
 
-
-  constructor(private _userSettingsService: UserSettingsService) {
-    this.userSettingsService = _userSettingsService;
-   }
-
-  ngOnInit() {
-    this.userSettingsService.SettingsChangeAsObservable().subscribe(message => {
-      // NoLog || console.log(this.ClassName + "receive message: " + message)
-      // let newSettings = this.userSettingsService.settings;
-      // NoLog || console.log(this.ClassName + "SCAO " + this.title, newSettings);
-      // this.customizeHeader(newSettings);
-    })
+  togglePanel() {
+    this.panelOpenState = !this.panelOpenState;
   }
 }
