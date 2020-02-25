d82e1a3ab172165a948f03c37e647003b0bc8e30
John Pankowicz <johnpankowicz@hotmail.com>
2020-02-25 14:36:17 -0500

    Remove uneeded styling from larg-cards

--- FrontEnd/ClientApp/src/app/dash-cards/large-cards/large-cards.ts
+++ FrontEnd/ClientApp/src/app/dash-cards/large-cards/large-cards.ts
@@ -3,28 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'gm-large-cards',
   template:`
-  <style>
-    $color-athens-gray: #EAEDF1;
-    $color-fiord: #394263;
-
-    .main {
-      //grid-area: main;
-      background-color: $color-athens-gray;
-      color: $color-fiord;
-
-      &__cards {
-        display: block;
-        column-count: 1;
-        column-gap: 20px;
-        margin: 20px;
-      }
-    }
-  </style>
   <div class="main__cards">
     <ng-content></ng-content>
   </div>
 `
-// styleUrls: ['./large-cards.scss']  // Why does it not work when the styles are in a file, instead of inline?
 })
 export class LargeCardsComponent implements OnInit {
 
